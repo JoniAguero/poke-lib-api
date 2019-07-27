7f5f29aa414d4c591d7028abddcbac7b50e19deb
Joni Agüero <aguerojonathanivan@gmail.com>
2019-07-27 08:05:13 -0300

    Cambios radicales

--- projects/poke-lib/src/lib/poke-lib.module.ts
+++ projects/poke-lib/src/lib/poke-lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ApiService } from './services/api/api.service';
 
 
 @NgModule({
   declarations: [],
   imports: [
   ],
-  exports: []
+  exports: [],
+  providers: [ApiService]
 })
 export class PokeLibModule { }
--- projects/poke-lib/src/public-api.ts
+++ projects/poke-lib/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of poke-lib
  */
 
+export * from './lib/services/index';
 export * from './lib/poke-lib.module';
