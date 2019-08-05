1890afc2ad00c3bb81269611997a6ce277381022
Joni Agüero <aguerojonathanivan@gmail.com>
2019-08-05 11:40:59 -0300

    fix coveralls?

--- projects/poke-lib/karma.conf.js
+++ projects/poke-lib/karma.conf.js
@@ -8,32 +8,34 @@ module.exports = function(config) {
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
       require("karma-coverage-istanbul-reporter"),
+      require('karma-mocha-reporter'),
       require("@angular-devkit/build-angular/plugins/karma")
     ],
-    client: {
+     client: {
+      captureConsole: false,
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "../../coverage"),
-      reports: ["html", "lcovonly", "text-summary"],
+      dir: require('path').join(__dirname, '../../coverage'),
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ["progress", "kjhtml"],
+    reporters: ['progress', 'mocha'],
+    mochaReporter: {
+      output: 'minimal'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromeHeadlessNoSandbox"],
-    // you can define custom flags
+    singleRun: false,
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"]
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
-    },
-    singleRun: true,
-    restartOnFileChange: true
+    }
   });
 };
